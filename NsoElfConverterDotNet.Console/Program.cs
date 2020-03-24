951fdf0ec732cb5b0b0524f2ba5d7eff72873c1d
Dixon, Evan <evanldixon@gmail.com>
2020-03-24 11:20:55 -0500

    Add wrapper around nx2elf

---
+++ NsoElfConverterDotNet.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NsoElfConverterDotNet.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: NsoElfConverterDotNet <main> <main.elf>");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Input file not found");
+                return;
+            }
+
+            Nx2Elf.NsoToElf(args[0], args[1]);
+        }
+    }
+}
---
+++ NsoElfConverterDotNet/Nx2Elf.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NsoElfConverterDotNet
+{
+    public static class Nx2Elf
+    {
+        const string DllFile = "nx2elf";
+
+        static Nx2Elf()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(Nx2Elf).Assembly, ImportResolver);
+        }
+
+        private static IntPtr ImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            IntPtr libHandle = IntPtr.Zero;
+            if (libraryName == DllFile)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    if (Environment.Is64BitOperatingSystem)
+                    {
+                        NativeLibrary.TryLoad("nx2elf-win-x64.dll", out libHandle);
+                    }
+                    else
+                    {
+                        NativeLibrary.TryLoad("nx2elf-win-x86.dll", out libHandle);
+                    }
+                }
+                else
+                {
+                    if (Environment.Is64BitOperatingSystem)
+                    {
+                        NativeLibrary.TryLoad("nx2elf-linux-x64.so", out libHandle);
+                    }
+                    else
+                    {
+                        NativeLibrary.TryLoad("nx2elf-linux-x86.so", out libHandle);
+                    }
+                }
+            }
+            return libHandle;
+        }
+
+        [DllImport(DllFile, EntryPoint = "NsoToElf")]
+        public static extern bool NsoToElf(string path, string elf_file, bool verbose = false);
+
+        //public static bool NsoToElf(string input, string output)
+        //{
+        //    IntPtr path = IntPtr.Zero;
+        //    IntPtr elf_file = IntPtr.Zero;
+        //    try
+        //    {
+        //        path = Marshal.StringToHGlobalUni(input);
+        //        elf_file = Marshal.StringToHGlobalUni(output);
+
+        //        return NsoToElf(path, elf_file, false);
+        //    }
+        //    finally
+        //    {
+        //        if (path != IntPtr.Zero)
+        //        {
+        //            Marshal.FreeHGlobal(path);
+        //        }
+        //        if (elf_file != IntPtr.Zero)
+        //        {
+        //            Marshal.FreeHGlobal(elf_file);
+        //        }
+        //    }
+        //}
+    }
+}
